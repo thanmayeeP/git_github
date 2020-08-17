3f6999675fb181d527195ef37b16f7efc55758c1
thanmayeeP <thanmaipriya1997@gmail.com>
2020-08-17 16:59:27 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping(path = "/get")
+	public String getHello() 
+	{
+		
+		System.out.println("Logs");
+		return "Hello World";
+		
+	}
+	
+	
+
+}
